94166e431ab5fbb8fb6f5d2dd400dca779301f4c
Tanmay <tanmaybonde10@gmail.com>
2026-01-20 11:53:43 +0530

    Dict

---
+++ DataStructure/Distnatory/dict.py
@@ -0,0 +1,18 @@
+# Dictionary
+dict1 = {}
+print(dict1)
+
+dict1 = {1: 'AI', 2: 'Microservices using java', 3: 'Cloud Computing'}
+print(dict1)
+
+dict1[3] = 'Cloud' #change the value
+print(dict1)
+
+dict1 = {'First': 'Lab on Python', 'second': 'Lab on Microservices', 'Third': 'Lab on AI'}
+print(dict1)
+
+dict1['Second'] = 'Next Generation Db'
+print(dict1)
+
+# Accessing
+print(dict1['First'])
--- DataStructure/Tuple/tuple1.py
+++ DataStructure/Tuple/tuple1.py
@@ -1,5 +1,30 @@
-tup = (2, "hii", 50.5, '@')
-
+# tup = (2, "hii", 50.5, '@')
 # tup[2] = 100 Cannot make changes here
 # print(tup)
+
+tuple = ()
+type(tuple)
+
+tuple = (1, 2, 3, 4, 5)
+print(tuple)
+
+tuple = (12)
+type(tuple) # it is considered as int
+print(tuple)
+
+#Accessing tuple
+
+tuple = (1, 2, 3, 4, 5, 'typle_example', 5.55)
+print(tuple)
+
+for t in tuple:
+    print(t)
+
+print(tuple[:])
+# print(tuple[5] [4])
+
+#Appending tuple
+
+tuple = tuple + (23, 45, 67)
+print(tuple)
 
