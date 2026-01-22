652f080ef873c8fe2327031ddbf1c19b556f2f3e
Tanmay <tanmaybonde10@gmail.com>
2026-01-22 15:28:24 +0530

    .

---
+++ String/string.py
@@ -0,0 +1,37 @@
+string1 = "Python Programming"
+
+name = "Python"
+print(name)
+
+message = "I love Python!"
+print(message)
+
+# Access string characters in python 
+# 1. Indexing
+message = 'hello'
+
+print(message[0])  # Output: 'h'
+print(message[1])  # Output: 'e'
+
+print(message[-4]) # Output: 'e
+
+# 2. Slicing
+
+message = "hello world"
+
+print(message[1:4]) #output 'ell
+
+#Strings are immutable
+message = 'Hello Everyone'
+# message[0] = 'M' 
+print(message) 
+
+message = 'Good Mornign'
+
+message = 'Hello everyone'
+print(message)
+
+# multiline string
+
+message = """ Basics is very important
+It forms the foundation for placement """
