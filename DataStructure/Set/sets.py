94b7a41058a4967d1147e66997882c84aede5b75
Tanmay <tanmaybonde10@gmail.com>
2026-01-29 15:10:17 +0530

    set

---
+++ DataStructure/Set/sets.py
@@ -0,0 +1,14 @@
+#set
+set_ds = {1, 3, 2, 4, 4, 5, 6, 7 ,2,  5, 7, 8}
+print(set_ds) # output {1, 2, 3, 4, 5}duplicates not allowed
+
+set_ds.add(55)
+print(set_ds)
+
+set_ds.remove(55)
+print(set_ds)
+
+#operations
+set_ds1 = {10,20,30,40,50}
+set_ds2 = {10,20,100,200,300}
+
---
+++ DataStructure/Set/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+set_ds.add(55)
+# print(set_ds)
+
+# set_ds.remove(55)
+# print(set_ds)
+
+# #operations
+# set_ds1 = {10,20,30,40,50}
+# set_ds2 = {10,20,100,200,300}
